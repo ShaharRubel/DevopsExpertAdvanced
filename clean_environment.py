3f2f0db69300559a6ee2d790e4a6ddb5ae460453
Shahar <shaharx18@gmail.com>
2025-03-07 19:00:11 +0200

    updated clean_environment.py

--- clean_environment.py
+++ clean_environment.py
@@ -1,6 +1,15 @@
 import requests
 
-# stop rest_app.py
-requests.get('http://127.0.0.1:5000/stop_server')
-# step web_app.py
-requests.get('http://127.0.0.1:5001/stop_server')
+try:
+    # stop rest_app.py
+    requests.get('http://127.0.0.1:5000/stop_server')
+    requests.get('http://127.0.0.1:5000/stop_server')
+except:
+    pass
+
+try:
+    # step web_app.py
+    requests.get('http://127.0.0.1:5001/stop_server')
+    requests.get('http://127.0.0.1:5001/stop_server')
+except:
+    pass
